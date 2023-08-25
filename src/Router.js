c03a07d5592c54c2c44cbf9827074270a1693303
Mehmetali Demirtas <82759834+mehmetalidemirtas@users.noreply.github.com>
2023-08-25 18:36:21 +0300

    react navigation

---
+++ src/Router.js
@@ -0,0 +1,38 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import PwGenerateScreen from './screens/PwGenerateScreen';
+import SavedScreen from './screens/SavedScreen';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={PwGenerateScreen} 
+        options={{
+            tabBarLabel: "Generate Password",
+            headerTitle:"Generate Password",
+            headerTitleAlign:"center",
+            tabBarIcon: () => (
+              <Icon name="lock-plus" color={"black"} size={25} />
+            ),
+          }} />
+        <Tab.Screen name="SavedScreen" component={SavedScreen}
+           options={{
+            tabBarLabel: "Saved Password",
+            presentation: 'transparentModal',
+            headerTitle:"Saved Password",
+            headerTitleAlign:"center",
+            tabBarIcon: () => (
+              <Icon name="bookmark" color={"black"} size={25} />
+            ),
+          }} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ src/screens/PwGenerateScreen/PwGenerateScreen.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
+const PwGenerateScreen = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  return (
+    <SafeAreaView>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}      />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic">
+        <View>
+        <Text>Password Wallet PwGenerateScreen</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default PwGenerateScreen;
